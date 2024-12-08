4c57406c92f8eb631f6de4415762869d3e471132
Eric Desoiza <ericdesoiza@linux2.cs.uchicago.edu>
2024-12-08 15:06:18 -0600

    fixed graph.c issues

--- graph.c
+++ graph.c
@@ -8,7 +8,7 @@ void print_graph(Graph *g){
   for (int i=0;i<g->nvertices;++i){              /* for each node 0..NV-1 */
     e = g->edges[i];                             /* e=pointer to root of node i's edge list */
     while (e != NULL) {                          /* traverse list */
-      printf("%d %d \n", i, e->y); /* print edge and weight */
+      printf("%d %d %d\n", i, e->y, e->weight); /* print edge and weight */
       e = e->next;
     }
   }
@@ -16,11 +16,25 @@ void print_graph(Graph *g){
 
 void read_graph(char *filename, Graph *g, bool directed){
   int nvertices, nedges;
-  int u,v;
+  int u,v, weight;
   FILE *file = NULL;
-  file = fopen(filename,"r");
-  fscanf(file,"%d %d", &nvertices, &nedges);     /* read header */
-  printf("NV = %d    NE = %d\n",nvertices,nedges);
+  char line[256];
+
+
+  file = fopen(filename, "r");
+  if (file == NULL) {
+    printf("Error opening file\n");
+    return;
+  }
+
+  while (fgets(line, sizeof(line), file) != NULL) {
+      if (line[0] == '#') {
+          continue;
+      }
+      sscanf(line, "%d %d", &nvertices, &nedges);
+      printf("NV = %d    NE = %d\n",nvertices,nedges);
+      break;
+  }
 
   g->nvertices = nvertices;                      /* initialize Graph fields */
   g->nedges = nedges;
@@ -31,17 +45,19 @@ void read_graph(char *filename, Graph *g, bool directed){
     g->edges[i] = NULL;
   
   Edgenode *prev;
-  while (fscanf(file, "%d %d \n", &u,&v) != EOF) {
+  while (fscanf(file, "%d %d %d\n", &u,&v, &weight) != EOF) {
     Edgenode *new_node = (Edgenode *) malloc(sizeof(Edgenode));
 
     if (g->edges[u] == NULL) {       /* if first edge foor vertex u */
       g->edges[u] = new_node;
       g->edges[u]->y = v;
+      g->edges[u]->weight = weight;
       g->edges[u]->next = NULL;
       prev = new_node;
     } else {                         /* adding another edge to vertex u */
       prev->next = new_node;
       new_node->y = v;
+      new_node->weight = weight;
       new_node->next = NULL;
       prev = new_node;
     }
